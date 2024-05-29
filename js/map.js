f2e44162155db085430018ed06af827749d19bbb
david <david@localhost.localdomain>
2024-05-29 20:56:58 +0200

    add search bar, layer controls and more

--- js/map.js
+++ js/map.js
@@ -1,3 +1,9 @@
+/*
+ * Café Map, David Hanny, ST 2024 
+ * PS Advanced Cartography and Geovisualisation
+ * University of Salzburg
+ */
+
 // create a new map with a fullscreen button
 var map = new L.Map('map', {
     fullscreenControl: true,
@@ -10,6 +16,8 @@ var map = new L.Map('map', {
 // set center and zoom to Salzburg
 map.setView([47.805, 13.04], 12.5);
 
+
+// ----------- Base layer ----------- //
 // Add a base layer, in my case CartoDB voyager. The basemap it is well-suited for navigation (which is important for cafés).
 // I don't really see the point of other basemap choices for my purpose.
 L.tileLayer('http://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}.png', {
@@ -21,21 +29,26 @@ L.tileLayer('http://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}.pn
 // to achieve reasonable popups, we need to loop over every feature and check which values are filled
 // also, we need it to build the main popup text
 
+
+// ----------- All cafés layer ----------- //
 // create a layer with clustering functionality
 var overviewLayer = L.markerClusterGroup({
-    maxClusterRadius: 40
+    maxClusterRadius: 45
 });
 
+// creates a marker with the coffee icon
+var coffeeMarker = L.AwesomeMarkers.icon({
+    icon: 'coffee',
+    iconColor: 'white',
+    prefix: 'fa',
+    markerColor: 'black'
+});
+    
 // add the markers to the layer
 var overviewMarkers = L.geoJson(cafes, {
     pointToLayer: function (feature, latlng) {
         return L.marker(latlng, {
-            icon: L.divIcon({
-                html: '<i class="fas fa-coffee fa-lg" style="color:black;"></i>',
-                className: 'custom-div-icon',
-                iconSize: [20, 20],
-                iconAnchor: [10, 10]
-            })
+            icon: coffeeMarker
         });
     },
     onEachFeature: function (feature, layer) {
@@ -56,11 +69,36 @@ var overviewMarkers = L.geoJson(cafes, {
 }).addTo(overviewLayer);
 map.addLayer(overviewLayer);
 
+
+// ----------- UI elements ----------- //
+// place zoom and fullscreen buttons at the top right
+map.zoomControl.setPosition('topright');
+map.fullscreenControl.setPosition('topright');
+
+// create a dictionary that stores the map layers and add a layer control
+var overlayMaps = {
+    'All Cafés': overviewLayer
+};
+var layerControl = L.control.layers(overlayMaps).addTo(map);
+
+// add a scalebar
+L.control.scale({
+    imperial: false,  // no ft here ;)
+    position: 'bottomleft'
+}).addTo(map);
+
+
+
+// ----------- Search bar ----------- //
+// add a searcg bar for the main layer
+map.addControl(new L.Control.Search({
+    layer: overviewLayer,
+    propertyName: 'name'
+}));
+
 /*
 TODO:
-- add a popup
 - add a search bar
-- add a layer switcher
 - add recommended layer
 - add a heatmap layer using https://github.com/Leaflet/Leaflet.heat?tab=readme-ov-file
 */
